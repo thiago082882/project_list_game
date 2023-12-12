d4948781b2035db2e913ec29879bdcc9a85b0644
thiago082882 <thiagocostadejesus2@gmail.com>
2023-12-11 22:57:59 -0300

    create project

---
+++ src/test/java/com/dev/thiago/ThiagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.thiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
